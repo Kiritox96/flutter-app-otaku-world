c039491fc370eac0a333c99c22e09e1f7e702f48
tammaro <tammaroimpicciatore@hotmail.com>
2020-01-19 01:59:48 +0100

    use anime unity for scraper

--- lib/anime.dart
+++ lib/anime.dart
@@ -4,6 +4,7 @@ import 'package:badges/badges.dart';
 import 'package:hello/util.dart';
 import 'package:hello/video.dart';
 import 'package:hive/hive.dart';
+import 'package:unity_ads_flutter/unity_ads_flutter.dart';
 
 import 'decoration.dart';
 import 'rest_api.dart';
@@ -14,44 +15,170 @@ class AnimePage extends StatefulWidget {
   _AnimePageState createState() => new _AnimePageState(anime);
 }
 
-class _AnimePageState extends State<AnimePage> {
-  
-       
-
+class _AnimePageState extends State<AnimePage> with UnityAdsListener{
+  UnityAdsError _error;
+  String _errorMessage;
+  bool _ready;
   _AnimePageState(this.anime);
   final dynamic anime;
-  
   Widget detail(dynamic an){
     return new Container(
-      width: MediaQuery.of(context).size.width / 3,
+
+      width: MediaQuery.of(context).size.width  ,
       margin: EdgeInsets.all(10.0),
-      child: Column(
+     // decoration: DecorationService.decEvidenza(),
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
+
           Container(
             height: 200,
-            width: MediaQuery.of(context).size.width / 2.5,
-            decoration: DecorationService.dec(an['image'])
+            margin: EdgeInsets.all(10.0),
+            decoration: DecorationService.dec(an['image']),
+            width: MediaQuery.of(context).size.width / 3.5,
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Text(an['episodi'].length.toString() + " episodi",textAlign:TextAlign.left,style: TextStyle(fontSize: 15.0,fontWeight: FontWeight.bold)),
-              ),
-              Container(
-                width:150,
-                margin: EdgeInsets.all(10.0),
-                child:  Text(an['generi'].join(' , '),textAlign:TextAlign.center,style: TextStyle(fontSize: 15.0,fontWeight: FontWeight.bold)) ,
-              ),
-              
-            ],
+          Container(
+            height: 200,
+             width: MediaQuery.of(context).size.width - (MediaQuery.of(context).size.width / 3.5) - 40,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(right: 10.0, left: 10.0, top: 10.0, bottom: 10.0),
+                  width: MediaQuery.of(context).size.width,
+                  child:Text("Trama",textAlign:TextAlign.left,style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold))
+                ),
+                Container(
+                  margin: EdgeInsets.only(right: 20.0, left: 10.0, top: 10.0, bottom: 0),
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(an['trama'],textAlign: TextAlign.justify,overflow: TextOverflow.ellipsis,style: TextStyle(fontSize: 12.0),maxLines: 10)
+                )
+              ],
+            )
           )
+         
+          
+
+          
         ],
-      ),
+      )
     );
   }
+  Container testo(String txt){
+    return new Container(
+      margin: EdgeInsets.only(right: 10.0, left: 10.0, top: 10.0, bottom: 10.0),
+      width: MediaQuery.of(context).size.width,
+      child:Text(txt,textAlign:TextAlign.left,style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold))
+    );
+  }
+  
+  String videoPlacementId='video';
+  
+  String gameIdAndroid='3427627';
+  String gameIdIOS='3427626';
+  @override
+  initState() {
+    UnityAdsFlutter.initialize(gameIdAndroid, gameIdIOS, this, true);
+    _ready = false;
+    super.initState();
+  }
+ @override
+  void onUnityAdsError(UnityAdsError error, String message) {
+    print('$error occurred: $message');
+    setState((){
+      this._error=error;
+      this._errorMessage=message;
+    });
+  }
+
+  @override
+  void onUnityAdsFinish(String placementId, FinishState result) {
+    print('Finished $placementId with $result');
+  }
+
+  @override
+  void onUnityAdsReady(String placementId) {
+    print('Ready: $placementId');
+    if (placementId == videoPlacementId){
+      setState(() {
+        this._ready=true;
+      });
+    }
+  }
+
+  @override
+  void onUnityAdsStart(String placementId) {
+    print('Start: $placementId');
+    if(placementId == videoPlacementId){
+      setState(() {
+        this._ready = false;
+      });
+    }
+  }
+  Widget episodi(dynamic an){
+    return new Container(
+      margin: EdgeInsets.only(right: 10.0, left: 10.0, top: 10.0, bottom: 10.0),
+      child:Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Divider(height: 2,color: Colors.black),
+          testo('Episodi'),
+          Container(
+            width: MediaQuery.of(context).size.width ,
+            height: 300,
+            child: gridView(an),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0, left: 10.0, top: 10.0, bottom: 10.0),
+            width: MediaQuery.of(context).size.width,
+            child:Text("Aggiungi ai preferiti",textAlign:TextAlign.right,style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold,color: Colors.blue[200]))
+          ),
+        ]
+      )
+    );
+  }
+  Widget gridView(dynamic an) {
+    return GridView.count(
+      crossAxisCount: 8,
+      children: List.generate(an['episodi'].length, (index) {
+        return Center(
+          child:GestureDetector(
+            onTap: (){
+              /*UtilService.showToast(
+                an['episodi'][index]
+              );*/
+              /*showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  // return object of type Dialog
+                  return AlertDialog(
+                    title: new Text("Alert Dialog title"),
+                    content: new Text(an['episodi'][index]),
+                    actions: <Widget>[
+                      // usually buttons at the bottom of the dialog
+                      new FlatButton(
+                        child: new Text("Close"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );*/
+              //String vid = an['episodi'][index]
+              Navigator.push(context, MaterialPageRoute(builder: (context) => VideoPage(an['episodi'][index].toString())));
+            },
+            child: Container(
+              child: Text((index + 1).toString(),style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold)),
+            ) 
+          )
+          
+        );
+      })
+    );
+  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -60,27 +187,28 @@ class _AnimePageState extends State<AnimePage> {
         title: new Text(this.anime['name'])
       ),
       body: new Container(
+        height: MediaQuery.of(context).size.height,
+
         child: animeDetail(this.anime)
       )
     );
   }
   Widget animeDetail(dynamic an){
-    return new SingleChildScrollView(
-      child: new Column(
-        children: [
-          ConnectionStatusBar(),
-          all(an)
-        ]
-      )
+    return  new Column(    
+      children: [
+        ConnectionStatusBar(),
+        all(an)
+      ]
     );
+  
   }
   Widget all(an){
     return new Container(
-      child: Row(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           detail(an),
-         
+          episodi(an)
         ],
       )
     );
--- lib/video.dart
+++ lib/video.dart
@@ -1,156 +1,85 @@
-import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
-import 'package:unity_ads_flutter/unity_ads_flutter.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 class VideoPage extends StatefulWidget {
-  final dynamic video;
-  VideoPage(dynamic video): this.video = video;
+  final String video;
+  final ChromeSafariBrowser browser =
+      new MyChromeSafariBrowser(new InAppBrowser());
+  VideoPage(String video): this.video = video;
   @override
   _VideoPageState createState() => new _VideoPageState(video);
 }
 
-class _VideoPageState extends State<VideoPage> with UnityAdsListener{
+class _VideoPageState extends State<VideoPage> {
 
-  _VideoPageState(this.video);
-  final dynamic video;
-  final Completer<WebViewController> _controller = Completer<WebViewController>();
+  InAppWebViewController webView;
+  String url;
+  double progress = 0;
 
-  String videoPlacementId='video';
-  int _ready = 0;
-  
-  String gameIdAndroid='3427627';
-  String gameIdIOS='3427626';
+  _VideoPageState(this.url);
+
   @override
-  initState() {
-    UnityAdsFlutter.initialize(gameIdAndroid, gameIdIOS, this, true);
-    _ready =0;
+  void initState() {
     super.initState();
   }
-  Widget landscape(){
-    return Container(
-      margin: EdgeInsets.only(right: 50.0, left: 50.0, top: 0, bottom: 0),
-      child: WebView(
-        initialUrl: this.video['episodio'][1],
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller.complete(webViewController);
-        }
-      )
-    );
 
+  @override
+  void dispose() {
+    super.dispose();
   }
   
-  Widget _floatingPanel(){
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white30,
-        borderRadius: BorderRadius.all(Radius.circular(24.0)),
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 20.0,
-            color: Colors.white70,
-          ),
-        ]
-      ),
-      margin: const EdgeInsets.all(24.0),
-      child: RotatedBox(
-        quarterTurns: 3,
-        child: Center(
-          child: Column(
-            children:[
-              Container(
-                width: 200.0,
-                margin: EdgeInsets.only(right: 10.0, left: 10.0, top: 20.0, bottom: 0),
-                child: new Text("SUB ITA",textAlign:TextAlign.center,style: TextStyle(color:Colors.blueAccent,fontSize: 18.0,fontWeight: FontWeight.bold))
-              ),
-              Container(
-                width: 200.0,
-                margin: EdgeInsets.only(right: 10.0, left: 10.0, top: 20.0, bottom: 0),
-                child: new Text("ITA",textAlign:TextAlign.center,style: TextStyle(color:Colors.blueAccent,fontSize: 18.0,fontWeight: FontWeight.bold))
-              ),
-              Container(
-                width: 200.0,
-                margin: EdgeInsets.only(right: 10.0, left: 10.0, top: 20.0, bottom: 0),
-                child: new Text("DOWNLOAD",textAlign:TextAlign.center,style: TextStyle(color:Colors.blueAccent,fontSize: 18.0,fontWeight: FontWeight.bold))
-              )
-            ]
-          )
-        ),
-      )
-    );
-  }
-  Widget _floatingCollapsed(){
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white30,
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0)),
-      ),
-      margin: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+  Widget landscape(){
+    return new Container(
+      margin: EdgeInsets.only(right: 50.0, left: 0, top: 50.0, bottom: 0),
+      //child:Text(this.video)
       child: Center(
-        child: new Icon(Icons.arrow_upward)
+        child: RaisedButton(
+        onPressed: () async {
+          await widget.browser.open(
+            url: "https://www.animeunityserver38.cloud/DDL/Anime/11eyes/11EyesMomoiroGenmutan_Ep_01_SUB_ITA.mp4",
+            options: ChromeSafariBrowserClassOptions(
+              androidChromeCustomTabsOptions: AndroidChromeCustomTabsOptions(addShareButton: false),
+              //ios: IOSSafariOptions(barCollapsingEnabled: true)
+            )
+          );
+        },
+        child: Text("Open Chrome Safari Browser")),
       )
     );
   }
-  BorderRadiusGeometry radius = BorderRadius.only(
-    topLeft: Radius.circular(24.0),
-    topRight: Radius.circular(24.0),
-  );
   @override
   Widget build(BuildContext context) {
-    if(this._ready == 0){
-      UnityAdsFlutter.show('video');
-    }
+    
     return new Scaffold(
-      body: SlidingUpPanel(
-        renderPanelSheet: false,
-        color: Color.fromRGBO(0, 0, 0, 100.0),
-        minHeight: 50.0,
-        maxHeight: 250.0,
-        collapsed: _floatingCollapsed(),
-        panel: _floatingPanel(),
-        body: Container(
-          decoration: BoxDecoration(color: Colors.black),
-          height: MediaQuery.of(context).size.height,
-          child: RotatedBox(
-            quarterTurns: 3,
-            child:landscape()
-          )
-        )
-      ),
+      body: Container(
+        //decoration: BoxDecoration(color: Colors.black),
+        height: MediaQuery.of(context).size.height,
+        //child: RotatedBox(
+          //quarterTurns: 3,
+          child:landscape()
+        //)
+      )
+      
 
     );
   }
+ 
+}
+class MyChromeSafariBrowser extends ChromeSafariBrowser {
+  MyChromeSafariBrowser(browserFallback) : super(bFallback: browserFallback);
+
   @override
-  void onUnityAdsError(UnityAdsError error, String message) {
-    print('$error occurred: $message');
-    
+  void onOpened() {
+    print("ChromeSafari browser opened");
   }
 
   @override
-  void onUnityAdsFinish(String placementId, FinishState result) {
-    print('Finished $placementId with $result');
+  void onLoaded() {
+    print("ChromeSafari browser loaded");
   }
 
   @override
-  void onUnityAdsReady(String placementId) {
-    print('Ready: $placementId');
-    /*if (placementId == videoPlacementId){
-      setState(() {
-        _ready++;
-      });
-    }*/
-  }
-
-  @override
-  void onUnityAdsStart(String placementId) {
-    print('Start: $placementId');
-    if(placementId == videoPlacementId){
-      setState(() {
-        _ready++;
-      });
-    }
+  void onClosed() {
+    print("ChromeSafari browser closed");
   }
 }
-
